d01839e44eb52c164d38b12a86ec970707c8095a
Lamine Dioubate <domanilamine96@gmail.com>
2021-04-23 17:30:16 -0300

    Design de Login et le Block

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator} from '@react-navigation/stack';
+
+const { Navigator, Screen} = createStackNavigator();
+
+//Importation des ecrans 
+import LoginScreen from './src/screens/LoginScreen';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello world! lamine</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator screenOptions={{headerShown: false}}>
+        <Screen name="Login" component={LoginScreen}/>
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+import Block from '../components/Block';
+
+const LoginScreen = (props) => {
+    const {container} = styles;
+    return (
+        <View style={container}>
+            <Block/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: "#fff",
+        
+    },
+});
+
+export default LoginScreen;
